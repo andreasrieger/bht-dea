fa5a40576d4969b78349e79a5bea31c8d7c422e9
andreasrieger <dev@rieger.digital>
2021-11-17 12:08:25 +0000

    output improvements

--- js/scripts.js
+++ js/scripts.js
@@ -2,103 +2,56 @@
  * 
  */
 
-const
-  sigma = ['B', 'E', 'P', 'S', 'T', 'V', 'X'], //the alphabet of words
-  proof = ['B', 'P', 'V', 'V', 'E'],
-  valid = "Eingabe akzeptiert",
-  invalid = "Eingabe verworfen"
-  ;
 
-var errorCount = 0;
-
-
-const genSeq = document.getElementById('generateSequence');
-const output = document.getElementById('output');
-const seqInput = document.getElementById('seqInput');
-const startDea = document.getElementById('startDea');
-const testDea = document.getElementById('testDea');
-const response = document.getElementById('response');
-const sigmaPlaceholder = document.getElementsByClassName('sigmaPlaceholder');
-const stategraph = document.getElementById('stategraph');
-
-
-if (genSeq) {
-  genSeq.addEventListener('click', () => {
-    const sequence = getRandomSequence();
-    seqInput.value = sequence;
-  })
-}
-
-testDea.addEventListener('click', () => {//fire button twice doubles the console output
-  const transitions = document.getElementById('transitions');
-  while (transitions.firstChild) {
-    transitions.removeChild(transitions.firstChild);
-  }
-  const machine = new Dea(proof);
-  if (machine[0]) runSequence(machine[0], machine[1]);
-})
-
-
-startDea.addEventListener('click', (event) => {
-  console.log(`seqInput: ${seqInput.value}`);
-
-  const transitions = document.getElementById('transitions');
-  while (transitions.firstChild) {
-    transitions.removeChild(transitions.firstChild);
-  }
-
-  // To do: check if empty
-  // To do: add commas to string
-  // To do: check string for compatibility with $sigma
-  // To do: running sequence according to the settings (auto, auto delayed, step by step)
-  const machine = new Dea(seqInput.value); //sequence comes from form
-  if (machine[0]) runSequence(machine[0], machine[1]);
-  else {
-    runSequence(machine[0], machine[1]);
-    errorCount += 1;
-  }
-  if (errorCount == 3) {
-    console.log("3x falsch!");
-    errorCount = 0;
-  }
-})
-
-
-
-
-function printStateTable(row, rowNum) {
+function printStateTableRow(row, rowNum) {
   const newRow = transitions.insertRow(-1);
   const th = document.createElement('th');
-  var thAttr = document.createAttribute('scope');
+  const thAttr = document.createAttribute('scope');
   thAttr.value = "row";
   th.setAttributeNode(thAttr);
   th.innerText = rowNum;
   newRow.appendChild(th)
   for (const element of row) {
     let newCell = newRow.insertCell(Object.keys(element) + 1);
-    // Append a text node to the cell
     let newText = document.createTextNode(element);
     newCell.appendChild(newText);
   }
 }
 
-function runStateGraph(output, delay) {
+
+function runAuto(output) {
+  for (let i = 0, l = output.length; i < l; i++) {
+    printStateTableRow(output[i], i += 1);
+  }
+}
+
+
+function runDelayed(output, delay) {
   // auto delayed
   for (let i = 0, l = output.length; i < l; i++) {
     setTimeout((y) => {
       console.log(output[y]);
-      printStateTable(output[y], y += 1);
+      printStateTableRow(output[y], y += 1);
     }, i * delay * 1000, i);
   }
 }
 
-function runSequence(valid, output) {
+
+function runOutput(valid, output) {
+
+  // To do: running sequence according to the settings (auto, auto delayed, step by step)
+  const autoRun = document.getElementById('startAuto');
   const oneSecondDelay = document.getElementById("1s");
   const twoSecondDelay = document.getElementById("2s");
   const fourSecondDelay = document.getElementById("4s");
-  oneSecondDelay.addEventListener('click', () => { runStateGraph(output, 1); });
-  twoSecondDelay.addEventListener('click', () => { runStateGraph(output, 2); });
-  fourSecondDelay.addEventListener('click', () => { runStateGraph(output, 4); });
+  const transitions = document.getElementById('transitions');
+  while (transitions.firstChild) {
+    transitions.removeChild(transitions.firstChild);
+  }
+  autoRun.addEventListener('click', () => { runAuto(output); });
+  oneSecondDelay.addEventListener('click', () => { runDelayed(output, 1); });
+  twoSecondDelay.addEventListener('click', () => { runDelayed(output, 2); });
+  fourSecondDelay.addEventListener('click', () => { runDelayed(output, 4); });
 }
 
 
@@ -113,18 +66,61 @@ const randomInt = (min, max) => {
 * Getting a random selection of x words from sigma.
 * X is a random number between min and max.
 */
-function getRandomSequence() {// To do: set min and max
+function getRandomSequence(alphabet) {// To do: set min and max
   const
-    n = sigma.length,
+    n = alphabet.length,
     arr = [],
     x = randomInt(5, 10)
     ;
 
   for (let i = 0; i < x; i++) {
-    arr.push(sigma[(0 + Math.floor(Math.random() * n)) % n]);
+    arr.push(alphabet[(0 + Math.floor(Math.random() * n)) % n]);
   }
   return arr;
 }
 
 
 // @Todo: develop a generator for valid sequences
+
+
+function startMachine(sequence) {
+  const machine = new Dea(sequence);
+  if (machine[0]) runOutput(machine[0], machine[1]);
+  else runOutput(machine[0], machine[1]);
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+  console.log("DOM fully loaded and parsed");
+  const genSeq = document.getElementById('generateSequence');
+  const seqInput = document.getElementById('seqInput');
+  const startDea = document.getElementById('startDea');
+  const testDea = document.getElementById('testDea');
+  const sigmaPlaceholder = document.getElementsByClassName('sigmaPlaceholder');
+  const stategraph = document.getElementById('stategraph');
+
+  const
+    sigma = ['B', 'E', 'P', 'S', 'T', 'V', 'X'],
+    proof = ['B', 'P', 'V', 'V', 'E']
+    ;
+
+  testDea.addEventListener('click', () => {
+    startMachine(proof); //fire button twice doubles the console output
+  })
+
+  if (genSeq) {
+    genSeq.addEventListener('click', () => {
+      seqInput.value = getRandomSequence(sigma);
+    })
+  }
+
+  startDea.addEventListener('click', (event) => {
+    console.log(`seqInput: ${seqInput.value}`);
+
+    // To do: check if empty
+    // To do: add commas to string
+    // To do: check string for compatibility with $sigma
+    startMachine(seqInput.value);
+  })
+
+})
