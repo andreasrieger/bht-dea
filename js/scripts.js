7a7387db6aded46b1b58e10c87f721482a610429
andreasrieger <29732816+andreasrieger@users.noreply.github.com>
2021-11-18 07:18:15 +0100

    further improvement

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@
 
 let output,
   step = 0;
+
 function printStateTableRow(row, rowNum) {
   const newRow = document.getElementById("transitions").insertRow(-1);
   const th = document.createElement("th");
@@ -21,13 +22,15 @@ function printStateTableRow(row, rowNum) {
 
 function clearUi() {
   const transitions = document.getElementById("transitions");
-  const seqInput = document.getElementById("seqInput");
+  // const seqInput = document.getElementById("seqInput");
+  document.getElementById("seqOutput").innerText = "";
+  document.getElementById("outputAll").setAttribute("disabled", "");
+  document.getElementById("outputDelayed").setAttribute("disabled", "");
+  document.getElementById("outputStepByStep").setAttribute("disabled", "");
 
   while (transitions.firstChild) {
     transitions.removeChild(transitions.firstChild);
   }
-
-  seqInput.setAttribute("valid", "false");
 
   step = 0;
 }
@@ -87,11 +90,18 @@ function getRandomSequence(alphabet) {
 // @Todo: develop a generator for valid sequences
 
 function startMachine(sequence) {
+  const outputText = document.getElementById("seqOutput");
   const machine = new Dea(sequence);
   if (machine[0]) {
     output = machine[1];
+    outputText.innerText = `Die Zeichenfolge "${sequence}" wurde akzeptiert`;
+    document.getElementById("outputAll").removeAttribute("disabled");
+    document.getElementById("outputDelayed").removeAttribute("disabled");
+    document.getElementById("outputStepByStep").removeAttribute("disabled");
+    document.getElementById("clear-control").removeAttribute("disabled");
   } else {
     console.log("Error");
+    outputText.innerText = `Die Zeichenfolge "${sequence}" wurde nicht akzeptiert`;
   }
 }
 
@@ -100,23 +110,27 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const genSeq = document.getElementById("generateSequence");
   const seqInput = document.getElementById("seqInput");
   const sigmaPlaceholder = document.getElementById("sigmaPlaceholder");
+  const startDea = document.getElementById("startDea");
   // const stategraph = document.getElementById("stategraph");
 
   const sigma = ["B", "E", "P", "S", "T", "V", "X"],
     proof = ["B", "P", "V", "V", "E"];
+    //BTSSXXTVVE
+    // BTXXVPSE
+    // BPVPXVPXVPXVVE
+    // BTSXXVPSE
 
   document.getElementById("test").addEventListener("click", function () {
     startMachine(proof);
   });
 
-  document.getElementById("clear").addEventListener("click", function () {
+  document.getElementById("clear-control").addEventListener("click", function () {
     clearUi();
   });
 
   if (seqInput) {
     document.getElementById("start").addEventListener("click", () => {
       seqInput.value = "";
-      seqInput.setAttribute("valid", "false");
     });
   }
 
@@ -130,13 +144,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
     });
   }
 
-  document.getElementById("startDea").addEventListener("click", (event) => {
+  startDea.addEventListener("click", (event) => {
     const input = seqInput.value;
-    const str = input.replace(",", "").toUpperCase();
+    const str = input.replace(",", "").toUpperCase();// To do: check for spaces and other invalid chars
 
-    startMachine(str);
+    startMachine(input);
 
-    const valid = () => {
+    /* const valid = () => {
       for (let i = 0, l = str.length; i < l; i++) {
         if (!sigma.includes(str[i])) return false;
       }
@@ -146,35 +160,37 @@ document.addEventListener("DOMContentLoaded", function (event) {
     // To do: check if empty
     if (str === "") {
       console.log("Feld ist leer!");
-      document.getElementById("startDea").classList.add("btn-danger");
+      startDea.classList.add("btn-danger");
     }
 
     // To do: check string for compatibility with $sigma
     else if (str !== "" && !valid()) {
       console.log(`Die Eingabe "${str}" enthält ungültige Zeichen!`);
-      if (!document.getElementById("startDea").classList.contains("btn-danger")){
-        document.getElementById("startDea").classList.add("btn-danger");
+      if (!startDea.classList.contains("btn-danger")){
+        startDea.classList.add("btn-danger");
       }
     }
 
     // prepare for take-off 
     else if (str !== "" && valid()){
-      document.getElementById("startDea").setAttribute("data-bs-dismiss", "modal");
-      if (document.getElementById("startDea").classList.contains("btn-danger")){
-        document.getElementById("startDea").classList.replace("btn-danger", "btn-success");
-      } else document.getElementById("startDea").classList.add("btn-success");
-      document.getElementById("startDea").innerText = "Starte Automaten";
-      seqInput.setAttribute("valid", "true");
+      startDea.setAttribute("data-bs-dismiss", "modal");
+      if (startDea.classList.contains("btn-danger")){
+        startDea.classList.replace("btn-danger", "btn-success");
+      } else startDea.classList.add("btn-success");
+      startDea.innerText = "Starte Automaten";
     }
 
     // take-off if ready
     else if (str !== "" && valid() && seqInput.getAttribute("valid") == true) {
       console.log(str);
       // startMachine(str);
-    }
+    } */
+
+
+
   });
 
-  document.getElementById("startAuto").addEventListener("click", runAuto);
+  document.getElementById("outputAll").addEventListener("click", runAuto);
 
   document.getElementById("1s").addEventListener("click", () => {
     runDelayed(1);
@@ -187,6 +203,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
 
   document
-    .getElementById("startStepByStep")
+    .getElementById("outputStepByStep")
     .addEventListener("click", runStepByStep);
 });
